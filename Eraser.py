1caafeb52841a3bf53bde60a5bded4b068dedcc7
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-11-06 09:38:13 +0530

    Update Eraser.py

--- Eraser.py
+++ Eraser.py
@@ -241,7 +241,7 @@ class Bot(discord.Client):
         if message.author == self.user or message.guild == None:
             return
 
-        if message.content.lower() == "hq":
+        if message.content.lower() == "hr":
             await message.delete()
             if BOT_OWNER_ROLE in [role.name for role in message.author.roles]:
                 self.embed_msg = None
