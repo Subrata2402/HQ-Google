6ca2d8ca0c4acd27740cee27ad9fc03157609ec8
Sakhman <68939201+Sakhman@users.noreply.github.com>
2021-02-05 06:46:31 +0530

    Update hqgoogle.py

--- hqgoogle.py
+++ hqgoogle.py
@@ -154,7 +154,7 @@ def connect_websocket(socket_url, auth_token):
                 embed.set_footer(text="HQ Google | Subrata#3297", icon_url="")
                 #embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/578379566544846901/630400208265805835/401ec468afa82a2937b8ad3a4e811463.jpg")
                 hook.send(embed=embed)
-                hook.send(message_data)
+                #hook.send(message_data)
                 #hook.send("+hp")
                 option1=f"{answers[0]}"
                 option2=f"{answers[1]}"
@@ -225,7 +225,7 @@ def connect_websocket(socket_url, auth_token):
                 embd.add_field(name="**__Status !__**", value=f"**● Advancing Players: {advancing} ({pA}%)**\n**● Eliminated  Players: {eliminated} ({pE}%)\n● Current Payout: ${payout}**", inline=True)
                 embd.set_footer(text=f"HQ Google | Subrata#3297", icon_url="")
                 hook.send(embed=embd)
-                hook.send(message_data)
+                #hook.send(message_data)
 
             elif message_data["type"] == "gameSummary":
                 winn = message_data['numWinners']
@@ -238,7 +238,7 @@ def connect_websocket(socket_url, auth_token):
                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/737764195743039488/737768505935659178/giphy1.gif")
                 embed.set_footer(text=f"HQ Google | Subrata#3297", icon_url="")
                 hook.send(embed=embed)
-                hook.send(message_data)
+                #hook.send(message_data)
 
 
 
