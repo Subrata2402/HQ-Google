7d5bb4fe7fdd7a0078f34564878de515f688a0d8
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-05-09 03:02:12 +0530

    Update hqgoogle.py

--- hqgoogle.py
+++ hqgoogle.py
@@ -170,21 +170,21 @@ def connect_websocket(socket_url, auth_token):
                     embed2.set_footer(text="HQ Google | HQ Friends")
                     hook.send(embed=embed2)
 
-            r = requests.get(google_query)
-            soup = BeautifulSoup(r.text , "html.parser")
-            result = soup.find("div" , class_='BNeawe').text
-            if option1 in result:
-                embed=Embed(title=f"**__Option 1. {option1}__**", description=result, color=0x00ffff)
-                hook.send(embed=embed)
-            elif option2 in result:
-                embed=Embed(title=f"**__Option 2. {option2}__**", description=result, color=0x00ffff)
-                hook.send(embed=embed)
-            elif option3 in result:
-                embed=Embed(title=f"**__Option 3. {option3}__**", description=result, color=0x00ffff)
-                hook.send(embed=embed)
-            else:
-                embed=Embed(title=f"**__Google Search Result !__**", description=result, color=0x00ffff)
-                hook.send(embed=embed)
+                r = requests.get(google_query)
+                soup = BeautifulSoup(r.text , "html.parser")
+                result = soup.find("div" , class_='BNeawe').text
+                if option1 in result:
+                    embed=Embed(title=f"**__Option 1. {option1}__**", description=result, color=0x00ffff)
+                    hook.send(embed=embed)
+                elif option2 in result:
+                    embed=Embed(title=f"**__Option 2. {option2}__**", description=result, color=0x00ffff)
+                    hook.send(embed=embed)
+                elif option3 in result:
+                    embed=Embed(title=f"**__Option 3. {option3}__**", description=result, color=0x00ffff)
+                    hook.send(embed=embed)
+                else:
+                    embed=Embed(title=f"**__Google Search Result !__**", description=result, color=0x00ffff)
+                    hook.send(embed=embed)
 
             elif message_data['type'] == 'answered':
                 name = message_data["username"]
