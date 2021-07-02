8dccdc7e5eb8bf771471e13b548c845c892e2188
Subrata2402 <68939201+Subrata2402@users.noreply.github.com>
2021-07-02 06:46:05 +0530

    Update hqgoogle.py

--- hqgoogle.py
+++ hqgoogle.py
@@ -21,7 +21,7 @@ from time import sleep
 
 
 
-webhook_url="https://discordapp.com/api/webhooks/838421331023233045/-AW8Pfv6d1wcHrn6npWAfTzmimAGFE-vxdjoHUiAjHmnTiXUe1OqpHDMOZghW8MrS15b"
+webhook_url="https://discordapp.com/api/webhooks/838850742575824966/-DN36GNIQgY3cVaESJI8aEE88BCrXoxrZWT0Z97KDe1I7-p4Fru5-tOudRcmq8JhTnRr"
 
 we="https://discordapp.com/api/webhooks/838850742575824966/-DN36GNIQgY3cVaESJI8aEE88BCrXoxrZWT0Z97KDe1I7-p4Fru5-tOudRcmq8JhTnRr"
 
